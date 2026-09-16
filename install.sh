d94494d547a79db8a0e5a7f7e78a9208cb0b2b06
Bartosz Szczeciński <54596452+syscore88@users.noreply.github.com>
2026-09-16 22:30:30 +0200

    update

--- install.sh
+++ install.sh
@@ -473,8 +473,6 @@ network-traffic=false
 autoconnect=1
 vm-window-size=(1280, 842)
 DCONFEOF
-else
-    log_warn "Brak polecenia dconf – pomijam wczytanie ustawień virt-managera." "dconf command not found – skipping virt-manager settings import."
 fi
 
 for svc in libvirtd virtqemud; do
